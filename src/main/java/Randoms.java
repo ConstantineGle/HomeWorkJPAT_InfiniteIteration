5eb1ceadec055130c2e3d335dcab11d79cd92374
glebov <constantine.glebov@gmail.com>
2023-03-06 10:29:37 +0700

    Initial commit

---
+++ src/main/java/Randoms.java
@@ -0,0 +1,28 @@
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    private Random random;
+    private int min;
+    private int max;
+
+    public Randoms(int min, int max) {
+        random = new Random();
+        this.min = min;
+        this.max = max + 1;
+    }
+
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt(max - min) + min;
+            }
+        };
+    }
+}
